16f8de80459d1babd4690481ff5a753cff25c726
DevCosmo <silentcosmo@gmail.com>
2022-11-18 04:25:04 +0530

    v.3.0.2

--- script.js
+++
@@ -1,8 +0,0 @@
-//$(document).ready(function(){
-    //$(".card").hover(function(){
-     // $("#ico").css("transform", "rotate(360deg)");
-     // }, function(){
-    //  $(this).css("background-color", "pink");
-   // });
- // });
- 
